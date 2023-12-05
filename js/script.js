378a942e7c8cc273bcd409713933ee06c3789b9a
Mindaugas Beleka <aiveris@gmail.com>
2023-12-05 14:54:15 +0100

    2023-12-05

--- js/script.js
+++ js/script.js
@@ -92,6 +92,3 @@ document.addEventListener("keydown", (event) => {
 function close_modal() {
   editModal.classList.remove("modal-show");
 }
-function drop_table() {
-  db.collection("a1").doc(`${doc.id}`).delete();
-}
